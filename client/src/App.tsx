bdb77f1f299d2c4fce97b2d61de8c19146f793d5
Tahmid Tapadar <o.tapadar@gmail.com>
2024-08-13 15:54:49 +0200

    CORS setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const [message, setMessage] = useState<string>('');
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/')
+            .then(response => response.json())
+            .then(data => setMessage(data.message));
+    }, []);
+
+    return (
+        <div>
+            <h1>{message}</h1>
+        </div>
+    );
+};
 
 export default App;
